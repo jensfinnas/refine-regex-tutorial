b5674650f34f04136ccfee1842fd6c52b464378e
jensfinnas <jens.finnas@gmail.com>
2014-03-24 09:16:53 +0100

    Create merge-xml-files.py

---
+++ merge-xml-files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Det här skriptet slår ihop alla xml-filer i samma mapp som skriptet ligger.
+
+import csv
+import glob
+
+# Hämta in alla xml-filer i mappen
+files = glob.glob("anforande-2013-mars2014/*.xml")
+print files
+
+# Definiera vad output-filen ska heta
+outFile = "output.xml"
+
+# Töm output-filen på innehåll
+fclear = open(outFile,"w")
+fclear.write("")
+fout = open(outFile,"a")
+
+# Skriv en första rad med ett samlande xml-objekt 
+fout.write("<data>")
+
+# Loop alla xml-filerna
+for fileName in files:
+	f = open(outFile)
+	# Loopa varje rad i xml-filen och skriv motsvarande rad till output-filen
+	for line in f:
+		fout.write(line)
+	f.close() # not really needed
+
+# Avsluta xml-filen
+fout.write("</data>")
+fout.close()
